754d6322123864751237728137516001657f0ca3
Sebastian <szxrocode@gmail.com>
2022-10-18 10:23:15 -0400

    Final Changes , more info see the documentation

--- Validatiors/UserValidator.cs
+++ Validatiors/UserValidator.cs
@@ -7,18 +7,39 @@ namespace Validatiors
     {
         public UserValidator()
         {
+            Include(new UserSimpleValidator());
+            Include(new UserComplexValidator()); 
+
+        }
+    }
+
+
+    public class UserNameValidator : AbstractValidator<UserName>
+    {
+        public UserNameValidator()
+        {
+          RuleFor(c => c.Name).NotEmpty().NotNull().WithMessage("The Child Fild have to be fill");
+        }
+    }
+
+    public class UserSimpleValidator : AbstractValidator<User> 
+    {
+        public UserSimpleValidator()
+        {
             RuleFor(c => c.Name).NotNull().NotEmpty().WithMessage("The Name field must be fill");
-           // RuleFor(c => c.Name).Must(x => x?.StartsWith("S") == true).WithMessage("The Name fill need a S");
+            // RuleFor(c => c.Name).Must(x => x?.StartsWith("S") == true).WithMessage("The Name fill need a S");
             RuleFor(c => c.Number).NotNull().WithMessage("The Number Field must be fill");
             RuleFor(c => c.Number).NotEqual(0).WithMessage("The Number must be higher than zero");
             RuleFor(c => c.LastName).NotEmpty().NotNull().WithMessage("The LastName Field must be fill");
+        }
+    }
 
-            //Children Rules(Array Rules)
+    public class UserComplexValidator : AbstractValidator<User>
+    {
+        public UserComplexValidator()
+        {
             RuleForEach(x => x.names)
-                    .ChildRules(x =>
-                    {
-                        x.RuleFor(c => c.Name).NotEmpty().NotNull().WithMessage("The Child Fild have to be fill");
-                    });
+                    .SetValidator(new UserNameValidator());
         }
     }
 }
